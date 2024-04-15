eaa20eb8ae4f4357e591637ba5870d99aad65695
qig <qig@veigit.com>
2024-04-15 14:18:33 +0800

    :art: opt: 优化proto编译脚本代码

--- compile-grpc.sh
+++ compile-grpc.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
+
 NODE_BIN_PATH=./node_modules/.bin
 OUT_DIR=./src/stores/grpc
-API_PROTO_FILES=$(find src/stores/grpc/$1 -name "*.proto" | grep -v "^src/stores/grpc/third_party" | grep "$2")
+
+# 默认搜索路径
+SEARCH_PATH="src/stores/grpc"
+# 如果提供了第一个参数，调整搜索路径
+if [ ! -z "$1" ]; then
+    SEARCH_PATH="${SEARCH_PATH}$1"
+fi
+
+# 查找文件的正则表达式
+PROTO_PATH_PATTERN="$SEARCH_PATH/.*\.proto$"
+if [ ! -z "$2" ]; then
+    PROTO_PATH_PATTERN="$PROTO_PATH_PATTERN|$SEARCH_PATH/third_party/$2/.*\.proto$"
+fi
+
+# 查找.proto文件
+API_PROTO_FILES=$(find $SEARCH_PATH -regex "$PROTO_PATH_PATTERN")
+
 for proto in $API_PROTO_FILES; do
-    echo compiling $proto...
+    echo "compling $proto..."
     $NODE_BIN_PATH/protoc \
-        --proto_path=${OUT_DIR} \
-        --proto_path=${OUT_DIR}/third_party \
-        --ts_out=${OUT_DIR} \
-        $proto
+    --proto_path=${OUT_DIR} \
+    --proto_path=${OUT_DIR}/third_party \
+    --ts_out=${OUT_DIR} \
+    $proto
     if [ $? -eq 0 ]; then
-        echo ok.
+        echo "ok."
+    else
+        echo "compling failed $proto"
     fi
 done
