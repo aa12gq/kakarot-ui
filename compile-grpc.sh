f248cb997e1be66abe6686b70c5354b01b7e3fe4
qig <qig@veigit.com>
2024-04-15 14:05:24 +0800

    :art:	chore:  proto编译脚本取消对third_party目录的忽略

--- compile-grpc.sh
+++ compile-grpc.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 NODE_BIN_PATH=./node_modules/.bin
 OUT_DIR=./src/stores/grpc
-API_PROTO_FILES=`find src/stores/grpc/$1 -path src/stores/grpc/third_party -prune -o -name "*.proto" | grep -v "^src/stores/grpc/third_party" | grep "$2"`
-
+API_PROTO_FILES=$(find src/stores/grpc/$1 -name "*.proto" | grep -v "^src/stores/grpc/third_party" | grep "$2")
 for proto in $API_PROTO_FILES; do
-      echo compling $proto...
-  		$NODE_BIN_PATH/protoc \
-  		--proto_path=${OUT_DIR}  \
-  		--proto_path=${OUT_DIR}/third_party \
-  		--ts_out=${OUT_DIR} \
-  		$proto
-  		if [ $? -eq 0 ]; then
-  		  echo ok.
-  		fi
+    echo compiling $proto...
+    $NODE_BIN_PATH/protoc \
+        --proto_path=${OUT_DIR} \
+        --proto_path=${OUT_DIR}/third_party \
+        --ts_out=${OUT_DIR} \
+        $proto
+    if [ $? -eq 0 ]; then
+        echo ok.
+    fi
 done
